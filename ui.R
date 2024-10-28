63fdac0d58952000f7a84d173eeb0b6558a1fcf1
akira1744 <akr1744@gmail.com>
2024-10-28 15:40:39 +0900

    TOPページ追加

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 ui <- 
   navbarPage(
-    title='DPC退院患者調査',
+    title='DPC退院患者調査分析',
     tabPanel(
       # Tab1
       title='実績比較',
@@ -10,12 +10,12 @@ ui <-
           width=2,
           selectInput(
             inputId='input_hp',
-            label=h4('注目病院'),
+            label=h4('注目病院を設定'),
             choices=list_hp,
             selected='全体'
           ),
           hr(),
-          h4('地域で絞り込み'),
+          h4('比較対象地域を設定'),
           selectInput(
             inputId='input_tiho',
             label='地方',
@@ -97,6 +97,63 @@ ui <-
             #   title='select_dpcmst',
             #   DTOutput('dt_select_dpcmst')
             # ),
+            tabPanel(
+              title='TOP',
+              h3('概要'),
+              p('本サイトは、DPC退院患者調査の結果を分析するためのサイトです。'),
+              p('病院ごとDPCごとの年間退院患者数を比較することができます。'),
+              p('現在,2018年度~2022年度の5年間分のデータを格納しています。'),
+              
+              h3('使用方法'),
+              h4('Step1: サイドバーで注目病院を設定してください'),
+              p('・注目病院の実績が太字で表示されるようになります。'),
+              p('・DPC大分類別(注目病院),DPC疾患分類別(注目病院),DPC手術分類類別(注目病院)の実績が表示されるようになります。'),
+              
+              h4('Step2: サイドバーで比較対象地域を設定してください'),
+              p('・比較対象設定のタブで、選択された施設を確認できます。'),
+              
+              h4('Step3: 実績確認'),
+              p('・タブを切り替えることで、深掘りして実績を確認することができます。'),
+              p('・病院別: 病院全体の実績'),
+              p('・DPC大分類別: DPC上2桁ごとの実績'),
+              p('・DPC疾患分類別: DPC上6桁ごとの実績'),
+              p('・DPC手術分類別: DPC上6桁 + DPC手術分類ごとの実績'),
+              
+              h3('データソース'),
+              a('令和４年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について',href='https://www.mhlw.go.jp/stf/shingi2/newpage_39119.html'),
+              br(),
+              a('令和３年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について',href='https://www.mhlw.go.jp/stf/shingi2/0000196043_00006.html'),
+              br(),
+              a('令和２年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について',href='https://www.mhlw.go.jp/stf/shingi2/0000196043_00005.html'),
+              br(),
+              a('令和元年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について',href='https://www.mhlw.go.jp/stf/shingi2/0000196043_00004.html'),
+              br(),
+              a('平成30年度DPC導入の影響評価に係る調査「退院患者調査」の結果報告について',href='https://www.mhlw.go.jp/stf/shingi2/0000196043_00003.html'),
+              br(),
+              p('例年通りであれば、令和5年度(2023年度)のデータは2025年3月下旬に公開されます。公開され次第アプリに反映します。'),
+              
+              h3('DPC退院患者調査データの注意点'),
+              h4('DPC退院患者調査の退院患者数は、実際の退院患者よりも少ないです。'),
+              p('・詳細は、「参考資料1:分析対象データ」、「参考資料2:集計条件について」をご参照ください。'),
+              a('・参考資料1:分析対象データ',href='https://www.mhlw.go.jp/content/12404000/001234082.pdf'),
+              br(),
+              a('・参考資料2:集計条件について',href='https://www.mhlw.go.jp/content/12404000/001234080.pdf'),
+              
+              h4('DPC退院患者調査に参加していない病院のデータは含まれていません。'),
+              p('DPC退院患者調査集計対象施設数は5922施設(2022年度)です。'),
+              p('DPCに該当しないケースは集計対象外です。'),
+              p('主に急性期病院の実績データとして解釈してください。'),
+              p('医療圏内シェア率は、あくまでDPC退院患者調査の範囲内でのシェア率を算出しておりますのでご注意ください。'),
+              
+              h3('問い合わせ先'),
+              HTML("
+                <ul>
+                  <li>Twitter: <a href='https://x.com/akira1744' target='_blank'>@akira1744</a></li>
+                  <li>GitHub: <a href='https://github.com/akira1744' target='_blank'>https://github.com/akira1744</a></li>
+                </ul>
+              "),
+              
+            ),
             tabPanel(
               title='病院別',
               DTOutput('dt_hp_table')
